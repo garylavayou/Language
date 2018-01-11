402c8d0f489dd3f5816ab1dc889ee40dab48b8cb
Gang Wang <gary.wang.uestc@outlook.com>
2018-01-11 17:06:33 +0800

    Modification
    change the handling of output information.

--- getstructfields.m
+++ getstructfields.m
@@ -1,13 +1,31 @@
 function new_struct = getstructfields( struct_obj, field_names, action, default_value)
 %getstructfields Retrieve fields from struct object.
 % |action|:
-%    'ignore': fields that does not exist in |struct_obj| will be ignored;
+%    'default': fields that does not exist in |struct_obj| or the field is empty, will be
+%           set as a specified default value (deault is 0);
 %    'empty': fields that does not exist in |struct_obj| will be set as empty;
-%    'default': fields that does not exist in |struct_obj| will be set as a specified
-%    default value (deault is 0); 
+%    'ignore': fields that does not exist in |struct_obj| or the field is empty will be
+%           ignored;
+%    'error': if a requested field is empty or not exist, produce an error.
 % multiple actions can be used in combination.
-% NOTE: invalid action will be ignored as no such action.
-if nargin < 3 
+% NOTE: invalid action will be ignored; 'error' has higher priority than 'ignore'.
+%% Debug Control
+% To control debug (warning),
+%
+% # decare the global |DEBUG| variable, ans use it to condition
+%    if ~isempty(DEBUG) && DEBUG
+%        debugging_code;
+%    end
+% # temporarily change the value of the global |DEBUG| variable in the caller, as follows,
+%
+%   global DEBUG;
+% 	old_debug = DEBUG;  % DEBUG might be empty.
+%   DEBUG = ture;       % to disable debug, set to false
+%   ...
+%   DEBUG = old_debug;
+global DEBUG;
+
+if nargin < 3
     action = '';
 end
 if contains(action, 'default') && nargin<4
@@ -19,6 +37,8 @@ end
 if ischar(field_names)      % to support a single char array (string).
     field_names = {field_names};
 end
+
+caller_name = calledby;
 new_struct = struct;
 for i = 1:length(field_names)
     if isfield(struct_obj, field_names{i})
@@ -29,9 +49,9 @@ for i = 1:length(field_names)
     end
     if contains(action, 'error')
         if isfield(struct_obj, field_names{i})
-            error('error: field ''%s'' is empty.', field_names{i});
+            error('%s: field ''%s'' is empty.', caller_name, field_names{i});
         else
-            error('error: field ''%s'' does not exist in struct.', field_names{i});
+            error('%s: field ''%s'' does not exist.', caller_name, field_names{i});
         end
     end
     if nargin >= 3 && contains(action, {'empty', 'default'})
@@ -42,28 +62,45 @@ for i = 1:length(field_names)
                 new_struct.(field_names{i}) = default_value{i};
             end
         else
-            new_struct.(field_names{i}) = default_value;
+            if length(field_names) == 1
+                new_struct.(field_names{i}) = default_value;
+            else
+                new_struct.(field_names{i}) = default_value(i);
+            end
         end
     end
     if ~contains(action, 'ignore')
         if isfield(struct_obj, field_names{i})
-            warning('field ''%s'' is empty and removed.', field_names{i});
+            message = sprintf('[%s] field ''%s'' is empty and removed.', caller_name, field_names{i});
         elseif isfield(new_struct, field_names{i})
-            if islogical(new_struct.(field_names{i})) ||...
-                    isnumeric(new_struct.(field_names{i}))
-                warning('field ''%s'' does not exist in struct, set as [%s].', ...
-                    field_names{i}, num2str(new_struct.(field_names{i})));
-            elseif ischar(new_struct.(field_names{i}))
-                warning('field ''%s'' does not exist in struct, set as [%s].', ...
-                    field_names{i}, new_struct.(field_names{i}));
+            value = new_struct.(field_names{i});
+            if ischar(value) ||  (isscalar(value) && (isnumeric(value) || islogical(value)))
+                val_str = evalc('disp(new_struct.(field_names{i}))');
+                message = sprintf('[%s] field ''%s'' does not exist, set as ''%s''.', ...
+                    caller_name, field_names{i}, strip(val_str));
             else
-                mc = metaclass(new_struct.(field_names{i}));
-                warning('field ''%s'' does not exist in struct, set as [%s].', ...
-                    field_names{i}, mc.Name);
+                val_str = evalc('disp(new_struct.(field_names{i}))');
+                val_str(end) = [];
+                message = sprintf('[%s] field ''%s'' does not exist, set as \n%s.', ...
+                    caller_name, field_names{i}, val_str);
             end
         else
-            warning('field ''%s'' does not exist in struct, ignored.', field_names{i});
+            message = sprintf('[%s] field ''%s'' does not exist, ignored.', ...
+                caller_name, field_names{i});
+        end
+        if ~isempty(DEBUG) && DEBUG
+            warning(message);
+        else
+            cprintf('SystemCommands', 'Warning: %s\n', message);
         end
     end
 end
 end
+
+% function str = logi2str(value)
+% if value
+%     str = 'true';
+% else
+%     str = 'false';
+% end
+% end
--- structmerge.m
+++ structmerge.m
@@ -1,8 +1,16 @@
 %% Merge multiple struct
 %    function sout = structmerge( s1, s2, ..., mode)
+% *mode*: 'override', 'exclude', 'warning';
+%       'override': replace the former value of the same field;
+%       'exclude': reject the later value the the same field;
+%       'warning': prompt information for duplicated fields;
 % NOTE: If two struct instances of the input have the same field and 'exclude' options is
 % not specified, the later will override the value of the former.
 function output_struct = structmerge( varargin )
+%% 
+% see <getstructfields> to set DEBUG information.
+global DEBUG;
+
 output_struct =struct;
 if ischar(varargin{end})
     mode = varargin{end};       % mode is 'override'|'exclude'
@@ -17,7 +25,7 @@ for i = 1:length(varargin)
     if isempty(varargin{i})
         continue;
     elseif ~isstruct(varargin{i})
-        error('error: structmerge: value is not a struct.');
+        error('error: %s: value is not a struct.', calledby);
     else
         exist_fields = fieldnames(varargin{i});
         for j = 1:length(exist_fields)
@@ -26,8 +34,14 @@ for i = 1:length(varargin)
                 output_struct.(exist_fields{j}) = varargin{i}.(exist_fields{j});
             end
             if b_duplicate && contains(mode, 'warning')
-                warning('structmerge: duplicate field (<%s> %s).', ...
+                message = sprintf('duplicate field (<%s> %s).', ...
                     exist_fields{j}, strtok(mode,'-'));
+                if ~isempty(DEBUG) && DEBUG
+                    % Decare the global DEBUG, or set a local DEBUG variable to enable warning.
+                    warning(message); %#ok<SPWRN>
+                else
+                    cprintf('SystemCommands', 'Warning: %s\n', message);
+                end
             end
         end
     end
