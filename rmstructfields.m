ca335b05c80ebaa87aa071c8722614c399f23c02
Gang Wang <gary.wang.uestc@outlook.com>
2018-01-29 15:38:51 +0800

    Minor

--- rmstructfields.m
+++ rmstructfields.m
@@ -1,6 +1,7 @@
 function [ sout ] = rmstructfields( sin , rm_fields)
 
 names = fieldnames(sin);
+sout = struct;
 for i = 1:length(names)
     if ~contains(names{i}, rm_fields)       % not exact, partial match will pass the exmination.
         sout.(names{i}) = sin.(names{i});
